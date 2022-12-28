d82aa17199861958b22ec2d5796fe430c85f9b24
sergey-salo <ssnonmail@gmail.com>
2022-12-28 13:41:00 +0200

    added 3 task to homework_14

--- homework_14/src/index.js
+++ homework_14/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 // 1.
 document.write(`<h3>1. Дано масив [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78] з елементами різних типів.
    Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.</h3>`);
@@ -43,7 +44,7 @@ const secondValue = +prompt('Введіть друге значення', 1);
 const operationSign = prompt('Введіть знак операціЇ (+, -, *, /, %, ^ (ступінь))', '+');
 
 function doMath(x, znak, y) {
-  const result = `${x} ${znak} ${y} = `
+  const result = `${x} ${znak} ${y} = `;
   switch (znak) {
     case '+':
       return result + (x + y);
@@ -64,3 +65,38 @@ function doMath(x, znak, y) {
 
 const mathResult = doMath(firstValue, operationSign, secondValue);
 document.write(`<p>Результат математичної дії: ${mathResult}</p>`);
+
+// 3.
+document.write(`<h3>3. Написати функцію заповнення даними користувача двомірного масиву.
+   Довжину основного масиву і внутрішніх масивів задає користувач.
+   Значення всіх елементів всіх масивів задає користувач.</h3>`);
+
+const mainArrayLenght = +prompt('Введіть довжину основного масиву');
+const internalArrayLenght = +prompt('Введіть довжину внутрішніх масивів');
+
+function createAndFillArray(mainArrLenght, internalArrLenght) {
+  const mainArray = new Array(mainArrLenght);
+
+  for (let i = 0; i < mainArray.length; i++) {
+    const innerArray = new Array(internalArrLenght);
+
+    for (let j = 0; j < innerArray.length; j++) {
+      innerArray[j] = prompt(`Введіть значення елементу масиву №[${i + 1}][${j + 1}]`);
+    }
+
+    mainArray[i] = innerArray;
+  }
+
+  return mainArray;
+}
+
+const outputArr = createAndFillArray(mainArrayLenght, internalArrayLenght);
+
+let resultStr = '';
+
+for (const item of outputArr) {
+  resultStr += `[${item.join(', ')}], </br>`;
+}
+
+resultStr = resultStr.slice(0, resultStr.length - 7);
+document.write(`<p>Результат: [</br>${resultStr}</br>]</p>`);
